2fd924128a4919e6f471056bef6dbda2ef88bd1e
mrjk <mrjk.78@gmail.com>
2022-10-07 05:27:05 +0200

    Update: DOcumentation for tag support

--- docs/gen_apidoc.sh
+++ docs/gen_apidoc.sh
@@ -6,6 +6,7 @@ python -m bash_kernel.install
 
 BUILD_DIR=./build
 BUILD_DIR=./src/schema_doc
+BUILD_DIR=./src/paasify_apidoc
 mkdir -p $BUILD_DIR
 
 paasify2 schema --format=json > $BUILD_DIR/paasify_yml_schema.json
@@ -18,3 +19,8 @@ generate-schema-doc --config-file doc_schema_md.yml $BUILD_DIR/paasify_yml_schem
 #generate-schema-doc --config-file doc_schema_html.yml $BUILD_DIR/paasify_yml_schema.json $BUILD_DIR/html
 #generate-schema-doc --config-file doc_schema_md.yml $BUILD_DIR/paasify_yml_schema.json $BUILD_DIR/md
 
+
+# Temporary project example !
+
+# Generate plugin documentation
+paasify2 --config ../examples/ex3/devbox_core/paasify.yml  explain --mode src/plugins_apidoc
---
+++ docs/src/plugins_apidoc/test-paasify-api/markdown.md
@@ -0,0 +1,26 @@
+
+# test-paasify-api
+
+Documentationfor tag: `test-paasify-api`
+
+## Meta data
+
+``` yaml
+api: 1
+author: mrjk
+description: Validate if the Paasify API works as expected
+email: ''
+license: ''
+name: Paasify API validator
+require: ''
+version: ''
+
+```
+
+## Tag documentation
+
+<iframe scrolling="yes" src="/plugins_apidoc/test-paasify-api/web.html" style="width: 100vw; height: 70vh; overflow: auto; border: 0px;">
+</iframe>
+
+
+                
